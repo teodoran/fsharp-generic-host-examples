f6ff7f5d6fe07bf178c33dbb369abd61ae6b29a1
Teodor Ande Elstad <teodor@andeelstad.com>
2023-04-26 16:15:42 +0200

    Common library for working with Task and Timer

---
+++ Library/Task.fs
@@ -0,0 +1,17 @@
+﻿module Example.Library.Task
+
+open System
+open System.Threading
+open System.Threading.Tasks
+
+let delay (delay: TimeSpan) (token: CancellationToken) =
+    task {
+        try
+            do! Task.Delay(delay, token)
+        with
+        | :? TaskCanceledException -> return ()
+        | :? ObjectDisposedException -> return ()
+        | error -> raise error
+    }
+
+let wait = delay Timeout.InfiniteTimeSpan
---
+++ Library/Timer.fs
@@ -0,0 +1,25 @@
+﻿module Example.Library.Timer
+
+open System
+open System.Threading
+open System.Threading.Tasks
+
+let create (run: DateTime -> unit) =
+    new Timer((fun _ -> run DateTime.Now), 0, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan)
+
+let start (period: TimeSpan) (token: CancellationToken) (timer: Timer) =
+    if token.IsCancellationRequested
+    then Task.CompletedTask
+    else
+        timer.Change(TimeSpan.Zero, period) |> ignore
+        Task.CompletedTask
+
+let stop (token: CancellationToken) (timer: Timer) =
+    if token.IsCancellationRequested
+    then Task.CompletedTask
+    else
+        timer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan) |> ignore
+        Task.CompletedTask
+
+let everySecond (run: DateTime -> unit) =
+    new Timer((fun _ -> run DateTime.Now), 0, TimeSpan.Zero, TimeSpan.FromSeconds 1)
